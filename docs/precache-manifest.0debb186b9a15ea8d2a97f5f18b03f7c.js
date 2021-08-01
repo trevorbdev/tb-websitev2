0569ac6703fc536378235d138ad79113db582324
Trevor Beurman <trevor@innov.dev>
2021-08-01 18:30:23 -0500

    tb website change to docs

---
+++ docs/precache-manifest.0debb186b9a15ea8d2a97f5f18b03f7c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "304dc7a79cc6047d2add",
+    "url": "/tb-websitev2/css/chunk-vendors.cbf35b18.css"
+  },
+  {
+    "revision": "8922dc55180b37d84ebea0c8b50baba6",
+    "url": "/tb-websitev2/img/Trevor_Beurman_small.8922dc55.png"
+  },
+  {
+    "revision": "b9891fdd8e06ee09c5e8fd58b45b4f77",
+    "url": "/tb-websitev2/img/the_joke_app_web.b9891fdd.png"
+  },
+  {
+    "revision": "f164c3793048d9e93e75044da9b5d45d",
+    "url": "/tb-websitev2/img/washburn_sbdc_web.f164c379.png"
+  },
+  {
+    "revision": "a8842524aadeb6fc501d122c7bb2860c",
+    "url": "/tb-websitev2/index.html"
+  },
+  {
+    "revision": "d83284dd59ed27c5e248",
+    "url": "/tb-websitev2/js/app.63941366.js"
+  },
+  {
+    "revision": "304dc7a79cc6047d2add",
+    "url": "/tb-websitev2/js/chunk-vendors.0fe87024.js"
+  },
+  {
+    "revision": "d5ea811cfeb6fc8db4cfab79a44977d6",
+    "url": "/tb-websitev2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tb-websitev2/robots.txt"
+  },
+  {
+    "revision": "6d862b7aa9b671158673b3fa529fe8b6",
+    "url": "/tb-websitev2/tb.cool.resume.pdf"
+  },
+  {
+    "revision": "f3642aa1f7204c48c89096728976d29e",
+    "url": "/tb-websitev2/tblogob.ico"
+  }
+]);
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  publicPath: "/tb-websitev2/"
+  publicPath: "/tb-websitev2/",
+  outputDir: "./docs"
 };
